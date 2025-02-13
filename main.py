deee972a7e7968f4a4759f76e90b37e4d9ce8e33
BrandonT <Btiseo@uncc.edu>
2025-02-13 15:54:07 -0500

    Final main.py Changes

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from bank_account import SavingAccount, CheckingAccount
 
 
-s1 = SavingAccount("Scoobert Boinkus", 90, 0, .10, 500)
-c1 = CheckingAccount("Dingle Klimberbob", 6000, 20, 50, 501)
+s1 = SavingAccount("Scoobert Boinkus", 1000, 500, .10, 500)
+s2 = SavingAccount("Alvin Tombunk", 1000, 500, .25, 501)
+c1 = CheckingAccount("Dingle Klimberbob", 6000, 20, 50, 502)
+c2 = CheckingAccount("Frodo Baggins", 50000, 5000, 1000, 503)
 
-print("CheckingAccount Withdraw")
-c1.print_customer_information()
-print("WITHDRAWING")
-c1.withdraw(5)
-c1.print_customer_information()
-
-print("CheckingAccount Withdraw FAIL")
-c1.print_customer_information()
-print("WITHDRAWING")
-c1.withdraw(51)
-
-print("SavingsAccount Interest Calculation")
+#Savings Account Testing
+print("S1 Interest Deposit")
 s1.print_customer_information()
-accrued = s1.calc_interest(4)
-print("DEPOSITING")
-print(f"I accrued ${accrued} in interest!")
+accrued = s1.calc_interest(5)
 s1.deposit(accrued)
 s1.print_customer_information()
+
+print("S2 Interest Deposit")
+s1.print_customer_information()
+accrued = s1.calc_interest(5)
+s1.deposit(accrued)
+s1.print_customer_information()
+
+print("C1 Withdraw - PASS")
+c1.print_customer_information()
+c1.withdraw(49)
+c1.print_customer_information()
+
+print("C2 Withdraw - FAIL")
+c2.print_customer_information()
+c2.withdraw(1001)
+
+
+
+
 
